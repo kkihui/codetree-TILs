df6cf85cf29ff2dfcebd75c90427a471b20de6ff
kkihui <121797755+kkihui@users.noreply.github.com>
2023-11-20 16:19:03 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231120/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Total days in Year";
+    cout << endl;
+    cout << 365;
+    cout << endl;
+    cout << "Circumference rate";
+    cout << endl;
+    cout << "3.1415926535"; // 따옴표 없이 그냥 float으로 넣으면 소수 점 5자리 까지 가능
+    return 0;
+}
