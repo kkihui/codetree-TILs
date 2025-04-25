d2cff6acec11bf49057a59341178f8afafad4fe0
kkihui <121797755+kkihui@users.noreply.github.com>
2025-04-25 16:18:13 +0900

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250424/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 7, b = 23, c = 30;
+    char plus = '+', equal = '=';
+    cout << a << " " << plus << " " << b << " " << equal << " " << c;
+
+    return 0;
+}
