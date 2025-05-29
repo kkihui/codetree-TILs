4de8f93e15ad0fd75ac1fa39b1ac7fa575707c32
kkihui <121797755+kkihui@users.noreply.github.com>
2025-05-29 14:37:00 +0900

    Title: 아스키코드표 맞추기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250529/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+    for (int i=0; i<5; i++)
+    {
+        cin >> arr[i];
+        cout << (char) arr[i] << ' ';
+    }
+    return 0;
+}
