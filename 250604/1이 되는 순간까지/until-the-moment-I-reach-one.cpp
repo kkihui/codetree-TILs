d6719cd6c852e650b726c20f1b97fe72c435935f
kkihui <121797755+kkihui@users.noreply.github.com>
2025-06-04 08:50:52 +0900

    Title: 1이 되는 순간까지, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250604/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int cnt = 0;
+
+int Until1(int num)
+{
+    if (num == 1)
+        return 0;
+    
+    if (num%2 == 0)
+        return Until1(num/2)+1;
+    else
+        return Until1(num/3)+1;
+}
+
+int main() {
+    cin >> N;
+
+    cout << Until1(N);
+
+    return 0;
+}
