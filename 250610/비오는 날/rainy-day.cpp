32cafceb58729768a1b4be866e878d10bcb6bc88
kkihui <121797755+kkihui@users.noreply.github.com>
2025-06-10 18:21:26 +0900

    Title: 비오는 날, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250610/비오는 날/rainy-day.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+#define Max_N 100
+
+using namespace std;
+
+int n;
+
+class WeatherInfo
+{
+    public:
+        string date, day, weather;
+
+        WeatherInfo(string ymd = " ", string d = " ", string w = " ")
+        {
+            this -> date = ymd;
+            this -> day = d;
+            this -> weather = w;
+        }
+};
+
+WeatherInfo infos[Max_N];
+
+int main() {
+    cin >> n;
+    int idx = -1;
+
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> infos[i].date >> infos[i].day >> infos[i].weather;
+        
+        if (infos[i].weather == "Rain")
+            {
+                if (idx == -1)
+                    idx = i;
+                else
+                {
+                    if (infos[i].date > infos[i].date)
+                        idx = i;
+                }
+            }
+    }
+
+    cout << infos[idx].date << " " << infos[idx].day << " " << infos[idx].weather;    
+    
+    return 0;
+}
