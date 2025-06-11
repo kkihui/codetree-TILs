326d44d2c2f362d8a8104b145822973fa86472b5
kkihui <121797755+kkihui@users.noreply.github.com>
2025-06-11 16:54:25 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250611/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+#define max_N 10
+
+using namespace std;
+
+class Person
+{
+    public:
+        string name;
+        int height, weight;
+
+        Person (string n = "", int h = 0, int w = 0)
+        {
+            this -> name = n;
+            this -> height = h;
+            this -> weight = w;
+        }
+};
+
+bool cmp(const Person &a, const Person &b)
+{
+    if (a.height == b.height)
+        return a.weight > b.weight;
+    return a.height < b.height;
+}
+
+Person People[max_N];
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cin >> People[i].name >> People[i].height >> People[i].weight;
+    
+    
+    sort(People,People+n,cmp);
+    for (int i = 0; i < 5; i++)
+        cout << People[i].name << " " << People[i].height << " " << People[i].weight << endl;
+    
+    return 0;
+}
