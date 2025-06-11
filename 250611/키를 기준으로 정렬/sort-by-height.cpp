6604b19fc9461d9de28b63cc185daaf1c416ac8b
kkihui <121797755+kkihui@users.noreply.github.com>
2025-06-11 14:10:39 +0900

    Title: 키를 기준으로 정렬, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250611/키를 기준으로 정렬/sort-by-height.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+#define max_N 10
+
+using namespace std;
+
+int n;
+
+class Person
+{
+    public:
+        string name;
+        int height;
+        int weight;
+        
+        Person(string n=" ", int h = 0, int w = 0)
+        {
+            this -> name = n;
+            this -> height = h;
+            this -> weight = w;
+        }
+};
+
+bool cmp (const Person &a, const Person &b)
+{
+    return a.height < b.height;
+}
+
+Person People[max_N];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cin >> People[i].name >> People[i].height >> People[i].weight;
+
+    sort(People,People+n,cmp);
+
+    for (int i = 0; i < n; i++)
+        cout << People[i].name << " " << People[i].height << " " << People[i].weight << endl;
+
+    return 0;
+}
