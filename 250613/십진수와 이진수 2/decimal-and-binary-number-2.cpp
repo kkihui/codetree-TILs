bdceeab9f69612d030002e660fb8c9c9bb89054b
kkihui <121797755+kkihui@users.noreply.github.com>
2025-06-13 17:01:08 +0900

    Title: 십진수와 이진수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250613/십진수와 이진수 2/decimal-and-binary-number-2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int Convert2Binary(int d)
+{
+    int result = 0, digit = 0;
+    int arr[20];
+
+    while (d >= 2)
+    {
+        arr[digit] = d % 2;
+        d /= 2;
+        digit++;
+    }
+    arr[digit] = d;
+
+    for (int i=digit; i >= 0; i--)
+    {
+        result += arr[i];
+        result *= 10;
+    }
+    result /= 10;
+
+    return result;
+}
+
+int Convert2Decimal(int b)
+{
+    int result = 0, square2 = 1, num;
+    while (b > 0)
+    {
+        num = b%10;
+        result += num*square2;
+        square2 *= 2;
+        b /= 10;
+    }
+    return result;
+}
+
+
+int main() {
+    int binary;
+    cin >> binary;
+
+    int decimal = Convert2Decimal(binary) * 17;
+
+    cout << Convert2Binary(decimal);
+
+    return 0;
+}
