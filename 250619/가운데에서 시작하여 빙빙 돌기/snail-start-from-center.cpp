29548e883fb6834376dc0015c509a3e125ad7da0
kkihui <121797755+kkihui@users.noreply.github.com>
2025-06-19 17:36:13 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250619/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int grid[100][100] = {0, };
+int dr[4] = {0,1,0,-1}, dc[4] = {-1,0,1,0};
+int dir_state = 0;
+
+
+bool InRange(int row, int col)
+{
+    return (0 <= row && row < n && 0 <= col && col < n);
+}
+
+bool CheckConti(int row, int col)
+{
+    int r, c;
+    for (int i = 0; i < 4; i++)
+    {
+        r = row + dr[i];
+        c = col + dc[i];
+        if (InRange(r,c) && grid[r][c] == 0)
+            return true;
+    }
+    return false;
+}
+
+void FillGrid()
+{
+    int r = n-1, c = n-1, nr, nc;
+    grid[r][c] = n*n;
+    int num = n*n-1;
+
+    while (CheckConti(r,c))
+    {
+        nr = r + dr[dir_state];
+        nc = c + dc[dir_state];
+        if (InRange(nr,nc) && grid[nr][nc] == 0)
+        {
+            r = nr; c = nc;
+            grid[r][c] = num--;
+        }
+        else
+            dir_state = (dir_state + 1) % 4;
+    }
+}
+
+void PrintGrid()
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << grid[i][j] << ' ';
+        cout << endl;
+    }
+}
+
+int main() 
+{
+    cin >> n;
+
+    FillGrid();
+
+    PrintGrid();
+
+    return 0;
+}
