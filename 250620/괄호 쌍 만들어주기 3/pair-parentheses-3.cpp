95073fe64ef7c4623a37937f7e05c2433b611f4d
kkihui <121797755+kkihui@users.noreply.github.com>
2025-06-20 02:17:37 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250620/괄호 쌍 만들어주기 3/pair-parentheses-3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string A;
+
+int main() 
+{
+    cin >> A;
+
+    int ans = 0;
+    for (int i = 0; i < A.length() - 1; i++)
+    {
+        if (A[i] == '(')
+        {
+            for (int j = i+1; j < A.length(); j++)
+            {
+                if (A[j] == ')')
+                    ans++;
+            }
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
