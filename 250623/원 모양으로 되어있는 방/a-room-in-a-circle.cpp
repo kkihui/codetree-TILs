14ad15fce177f46a068954367f0f4aba79366114
kkihui <121797755+kkihui@users.noreply.github.com>
2025-06-23 13:16:25 +0900

    Title: 원 모양으로 되어있는 방, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250623/원 모양으로 되어있는 방/a-room-in-a-circle.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int a[1003];
+
+int Min(int n1, int n2)
+{
+    if (n1 > n2)
+        return n2;
+    return n1;
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int ans = 100300;
+    // start i th room
+    for (int i = 0; i < n; i++)
+    {
+        int dist_sum = 0;
+        // calculate distance for all rooms
+        for (int j = 0; j < n; j++)
+            dist_sum += a[(i+j)%n] * j;
+        ans = Min(ans,dist_sum);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
