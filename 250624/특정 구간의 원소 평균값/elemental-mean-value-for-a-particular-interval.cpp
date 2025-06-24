7a6ac8498c3a4e9b5698bc2942824d6930f405e3
kkihui <121797755+kkihui@users.noreply.github.com>
2025-06-24 13:47:23 +0900

    Title: 특정 구간의 원소 평균값, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250624/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+double arr[100];
+
+int main() 
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int avg_cnt = n;
+
+    // 구간 크기 2 ~ n까지 해보기
+    for (int size = 2; size <= n; size++)
+    {
+        for (int i = 0; i <= n - size; i++)
+        {
+            double avg = 0;
+            for (int j = 0; j < size; j++)
+                avg += arr[i+j];
+            avg /= size;
+
+            for (int idx = 0; idx < n; idx++)
+            {
+                if (avg == arr[idx])
+                {
+                    avg_cnt++;
+                    break;
+                }
+            }
+        }
+    }
+
+    cout << avg_cnt << endl;
+
+    return 0;
+}
