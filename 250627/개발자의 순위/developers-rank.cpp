b1e9fcd9e2231255e64c2e20d2f11c620cec3ac6
kkihui <121797755+kkihui@users.noreply.github.com>
2025-06-27 14:25:50 +0900

    Title: 개발자의 순위, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250627/개발자의 순위/developers-rank.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int k, n;
+int arr[10][20];
+
+bool IsFast(int a, int b)
+{
+    for (int i = 0; i < k; i++)
+    {
+        bool is_fast = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i][j] == b)
+            {
+                if (!is_fast)
+                {
+                    return false;
+                }
+            }
+            else if (arr[i][j] == a)
+            {
+                is_fast = 1;
+            }
+        }
+    }
+    return true;
+}
+
+int main() 
+{
+    cin >> k >> n;
+
+    for (int i = 0; i < k; i++)
+        for (int j = 0; j < n; j++) cin >> arr[i][j];
+    
+    int ans = 0;
+    for (int a = 0; a < n; a++)
+    {
+        for (int b = 0; b < n; b++)
+        {
+            if (a != b)
+            {
+                if (IsFast(a,b))
+                {
+                    ans++;
+                }
+            }
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
