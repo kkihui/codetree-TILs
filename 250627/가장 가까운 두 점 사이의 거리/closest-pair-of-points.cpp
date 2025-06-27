6c4376625a47a33e31cb9d58fd589d2f2704e90a
kkihui <121797755+kkihui@users.noreply.github.com>
2025-06-27 12:54:50 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250627/가장 가까운 두 점 사이의 거리/closest-pair-of-points.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int n;
+int x[100], y[100];
+
+int CalDistance(int x1, int y1, int x2, int y2)
+{
+    return (x2-x1)*(x2-x1) + (y2-y1) * (y2-y1);
+}
+
+int main() 
+{
+    cin >> n;
+
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> x[i] >> y[i];
+    }
+
+    int ans = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            int dist = CalDistance(x[i],y[i],x[j],y[j]);
+            ans = min(ans,dist);
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
